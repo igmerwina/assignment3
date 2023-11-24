35eb4c114148234864e2c834b1c97c14ebe24733
P93694-dev <erwin.ardiantha@pegadaian.co.id>
2023-11-24 10:37:25 +0700

    [REFACTOR] - separate function

--- service/update.go
+++ service/update.go
@@ -9,13 +9,12 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
-	"time"
 
 	"github.com/labstack/echo/v4"
 )
 
 func UpdateData(c echo.Context) error {
-	log.Println("--> Execute update data water & wind\n", time.Now())
+	log.Println("--> Execute update data water & wind")
 
 	water := model.WaterStatus{
 		Water: rand.Intn(15) + 1,
@@ -46,36 +45,7 @@ func UpdateData(c echo.Context) error {
 		return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Failed to save data"})
 	}
 
-	// Get the last element in the array
-	var statusWater string = ""
-	var statusWind string = ""
-	if len(wotah) > 0 {
-		lastStatus := wotah[len(wotah)-1]
-		switch {
-		case lastStatus.Water < 5:
-			statusWater = "Aman"
-		case lastStatus.Water >= 6 && lastStatus.Water <= 8:
-			statusWater = "Siaga"
-		case lastStatus.Water > 8:
-			statusWater = "Bahaya"
-		}
+	util.GetStatus(wotah)
 
-		switch {
-		case lastStatus.Wind < 6:
-			statusWind = "Aman"
-		case lastStatus.Wind >= 7 && lastStatus.Wind <= 15:
-			statusWind = "Siaga"
-		case lastStatus.Wind > 15:
-			statusWind = "Bahaya"
-		}
-
-		fmt.Printf("Water: %#v", lastStatus.Water)
-		fmt.Printf(" - Status: %s\n", statusWater)
-		fmt.Printf("Wind: %#v", lastStatus.Wind)
-		fmt.Printf(" - Status: %s\n", statusWind)
-	} else {
-		fmt.Println("No data in the JSON file.")
-	}
-
-	return c.JSON(http.StatusOK, map[string]string{"message": "Data saved successfully "})
+	return c.JSON(http.StatusOK, nil)
 }
